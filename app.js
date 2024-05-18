df23fc86ec022a4f969816cf43f66c3addf13fca
muhammadafroz7777 <153561186+muhammadafroz7777@users.noreply.github.com>
2024-05-18 19:02:13 +0500

    done

---
+++ app.js
@@ -0,0 +1,24 @@
+var text = document.getElementById("text");
+var bold = document.getElementById("bold");
+var Italic = document.getElementById("Italic");
+var Underline = document.getElementById("U");
+
+
+
+function myFunction() {
+    document.getElementById("text").style.fontFamily = "Italic";
+  }
+
+  function mybold() {
+    document.getElementById("text").style.fontWeight = "bold";
+  }
+
+  function myUnderline() {
+    document.getElementById("text").style.textDecoration = "Underline";
+  }
+
+
+
+
+
+
